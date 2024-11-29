67988e3167a5b8c3b365d4b6c75ac17379eac136
WrightWayClean <wrightwaycleanid@gmail.com>
2024-11-28 18:58:28 -0700

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $recaptchaResponse = $_POST['g-recaptcha-response'];
 
     // Verify reCAPTCHA
-    $secretKey = 'YOUR_SECRET_KEY_HERE';  // Insert your secret key
+    $secretKey = '6LfvJFIqAAAAAM3Yc8EiS9sYiEzFav4rzEk_ruaN';
     $verifyResponse = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secretKey&response=$recaptchaResponse");
     $responseData = json_decode($verifyResponse);
 
