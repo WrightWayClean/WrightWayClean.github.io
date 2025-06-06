34624bf662f0a10ceda830607b017a221bd81a52
WrightWayClean <wrightwaycleanid@gmail.com>
2025-06-06 11:53:49 -0600

    Delete access-control.js

--- access-control.js
+++
@@ -1,9 +0,0 @@
-fetch("https://api64.ipify.org?format=json")
-.then(response => response.json())
-.then(data => {
-    let allowedIPs = ["160.2.132.79"]; // Add all approved IPs
-    if (!allowedIPs.includes(data.ip)) {
-        window.location.replace("/404.html"); // Redirect unauthorized users to the fake 404 page
-    }
-})
-.catch(error => console.error("Error fetching IP:", error));
